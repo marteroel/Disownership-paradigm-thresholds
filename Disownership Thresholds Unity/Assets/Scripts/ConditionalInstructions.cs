877ec0f7810c893d8642910c17e912d28a38b256
marteroel <marteroel@users.noreply.github.com>
2019-05-03 12:15:27 +0200

    Changed most instructions to German, changed position of user canvases

---
+++ Disownership Thresholds Unity/Assets/Scripts/ConditionalInstructions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleVAS;
+using UnityEngine.UI;
+
+public class ConditionalInstructions : MonoBehaviour {
+
+    public Text instructionsA, instructionsB;
+
+	// Use this for initialization
+	void Start () {
+        if (BasicDataConfigurations.condition == "Active movement")
+            instructionsA.gameObject.SetActive(true);
+        else
+            instructionsB.gameObject.SetActive(true);
+	}
+}
