9727661db495b29ef2cad75334b1334f7386b5fc
LazarusSpalanzani <37871736+LazarusSpalanzani@users.noreply.github.com>
2019-10-16 18:59:30 +0200

    Added timed autostart inbetween trials

---
+++ Disownership Thresholds Unity/Assets/Packages/Simple VAS VR/SimpleVAS/Scripts/LoadSceneOnTime.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using WebcamDelay;
+
+namespace SimpleVAS
+{
+
+    public class LoadSceneOnTime : MonoBehaviour
+    {
+
+        public string sceneToLoad;
+        public float waitingTime;
+
+        // Use this for initialization
+        void Start()
+        {
+            StartCoroutine("ChangeSceneAtTime");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private IEnumerator ChangeSceneAtTime()
+        {
+
+            yield return new WaitForFixedTime(waitingTime);
+            SceneManager.LoadScene(sceneToLoad);
+
+        }
+
+    }
+}
