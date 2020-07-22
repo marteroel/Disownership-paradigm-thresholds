e090ad3419b387e24e290305b27a0c7fa1842991
marteroel <marteroel@users.noreply.github.com>
2020-07-22 17:03:42 +0200

    Added WaitToLoadScene, for inter scene to stay for a few seconds before beginning trials

---
+++ Assets/Scripts/WaitToLoadScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using SimpleVAS;
+
+public class WaitToLoadScene : MonoBehaviour
+{
+
+    public string sceneToLoad;
+    public float secondsToWait;
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown("space"))
+            StartCoroutine(LoadAfterPeriod(secondsToWait));
+    }
+
+
+    private IEnumerator LoadAfterPeriod(float time){
+
+        yield return new WaitForSecondsRealtime(time);
+
+        if (sceneToLoad != "")
+            SceneManager.LoadScene(sceneToLoad);
+        else
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
+
