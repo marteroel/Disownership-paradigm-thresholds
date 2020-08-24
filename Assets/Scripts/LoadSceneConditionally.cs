7f978d4a0aa416697207e426b46fdc2819acc152
marteroel <marteroel@users.noreply.github.com>
2020-08-24 14:11:11 +0200

    Now allows to skip to final questionnaire

---
+++ Assets/Scripts/LoadSceneConditionally.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadSceneConditionally : MonoBehaviour {
+
+    public string sceneToLoadOnFalse, sceneToLoadOnTrue;
+    public Toggle _toggle;
+    
+
+        public void OnNextButton()
+        {
+
+            if (!_toggle.isOn)
+                SceneManager.LoadScene(sceneToLoadOnFalse);
+            else
+            SceneManager.LoadScene(sceneToLoadOnTrue);
+    }
+    }
