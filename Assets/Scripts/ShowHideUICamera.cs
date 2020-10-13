4a6a0196d9164936fc37ead163ea94e6d551c0b6
marteroel <marteroel@users.noreply.github.com>
2020-10-13 14:57:11 +0200

    Hide/show UI radial from inspector

---
+++ Assets/Scripts/ShowHideUICamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ShowHideUICamera : MonoBehaviour {
+
+
+    public GameObject uireticle;
+    public static ShowHideUICamera instance;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+    }
+
+    // Use this for initialization
+    public void ShowOrHide(bool show){
+        if (show){
+            uireticle.SetActive(true);
+        }
+        else
+        {
+            uireticle.SetActive(false);
+        }
+    }
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+    public bool show;
+	// Use this for initialization
+	void Awake () {
+        ShowHideUICamera.instance.ShowOrHide(show);
+	}
+
+}
